88bae899a78b1e823ea1a86173dd56b8c42e7ca9
Svetozar Stoev <zarkoto23@gmail.com>
2024-11-17 17:58:51 +0200

    solutions

--- Advanced/dom introduction exersice/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ Advanced/dom introduction exersice/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -1,8 +1,30 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   function onClick() {
-      //   TODO:
+  function onClick() {
+    const studentListRows = document.querySelectorAll("table tbody tr");
+    const searchFieldEl = document.querySelector("#searchField");
+    const searchStr = searchFieldEl.value.toLowerCase();
 
-   }
+    const studentsList = Array.from(studentListRows).map((el) => el.innerText.toLowerCase());
+
+    if(searchStr==''){ return;
+    }
+    const result = [];
+    studentListRows.forEach((el) => {
+      el.classList.remove('select');
+    });
+
+    for (let i = 0; i < studentsList.length; i++) {
+      if (studentsList[i].indexOf(searchStr) >= 0) {
+        result.push(i);
+      }
+    }
+    result.forEach((position, index) => {
+      studentListRows[position].classList.add('select');
+    });
+    searchFieldEl.value = "";
+
+    console.log(result);
+  }
 }
--- Advanced/dom introduction exersice/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ Advanced/dom introduction exersice/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO
+  const inputEl = document.querySelector("#input");
+  const outputEl = document.querySelector("#output");
+  const buttonEl = document.querySelector("#formatBtn"); //
+
+  if (inputEl.value == "") {
+    return;
+  }
+
+  const sentencesPerParagraph = 3;
+  const sentences = inputEl.value.split(". ");
+  const numberOfParagraph = Math.ceil(sentences.length / sentencesPerParagraph);
+
+  let output = "";
+  for (let i = 0; i < numberOfParagraph; i++) {
+    const p = i * numberOfParagraph;
+    const text = sentences.slice(p, p + sentencesPerParagraph).join(". ");
+    output += `<p>${text}</p>`;
+  }
+
+  outputEl.innerHTML = output;
 }
