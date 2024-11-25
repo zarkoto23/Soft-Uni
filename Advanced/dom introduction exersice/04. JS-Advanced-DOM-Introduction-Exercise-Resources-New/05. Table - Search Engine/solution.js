60b7d1e2adcc6b5e6a4d0b0115b52709bcda0e08
Svetozar Stoev <zarkoto23@gmail.com>
2024-11-25 17:00:08 +0200

    solve searchEngine

--- Advanced/dom introduction exersice/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ Advanced/dom introduction exersice/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -2,29 +2,30 @@ function solve() {
   document.querySelector("#searchBtn").addEventListener("click", onClick);
 
   function onClick() {
-    const studentListRows = document.querySelectorAll("table tbody tr");
-    const searchFieldEl = document.querySelector("#searchField");
-    const searchStr = searchFieldEl.value.toLowerCase();
+ const trElements=document.querySelectorAll('tbody tr')
+const searchInput=document.querySelector('#searchField')
+const searchStr=searchInput.value.toLowerCase()
+const result=[]
+const info=[...trElements].map(el=> el.innerText.toLowerCase())
 
-    const studentsList = Array.from(studentListRows).map((el) => el.innerText.toLowerCase());
+trElements.forEach(el=>{
+  el.classList.remove('select')
+})
 
-    if(searchStr==''){ return;
-    }
-    const result = [];
-    studentListRows.forEach((el) => {
-      el.classList.remove('select');
-    });
+if(searchStr=='') return
 
-    for (let i = 0; i < studentsList.length; i++) {
-      if (studentsList[i].indexOf(searchStr) >= 0) {
-        result.push(i);
-      }
-    }
-    result.forEach((position, index) => {
-      studentListRows[position].classList.add('select');
-    });
-    searchFieldEl.value = "";
+for(let i=0;i<info.length;i++){
+  if(info[i].indexOf(searchStr)>=0){ 
+    result.push(i)
+  }
+}
+result.forEach((position)=>{
+  trElements[position].classList.add('select')
+})
+searchInput.value=''
 
-    console.log(result);
+
+console.log(result)
+
   }
 }
