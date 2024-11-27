aae3bf98db7416fb304a62f82b3e52ed12e7e1ca
Svetozar Stoev <zarkoto23@gmail.com>
2024-11-27 15:30:16 +0200

    number convertor

--- Advanced/dom introduction exersice/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
+++ Advanced/dom introduction exersice/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
@@ -1,4 +1,23 @@
 function solve() {
+  const output = document.querySelector("#result");
+  const selectElConvert = document.querySelector("#selectMenuTo");
+  const optionSelectElConvertTo = document.createElement("option");
+  optionSelectElConvertTo.setAttribute("value", "hexadecimal");
+  optionSelectElConvertTo.textContent = "Hexadecimal";
 
-    //TODO...
+  const optionSelectElConvertToDec = document.createElement("option");
+  optionSelectElConvertToDec.setAttribute("value", "binary");
+  optionSelectElConvertToDec.textContent = "Binary";
+
+  selectElConvert.appendChild(optionSelectElConvertToDec);
+  selectElConvert.appendChild(optionSelectElConvertTo);
+
+  document.querySelector("#container button").addEventListener("click", (e) => {
+    const input = Number(document.querySelector("#input").value);
+    if (selectElConvert.value == "binary") {
+      output.value = input.toString(2);
+    } else if (selectElConvert.value == "hexadecimal") {
+      output.value = input.toString(16).toUpperCase();
+    }
+  });
 }
