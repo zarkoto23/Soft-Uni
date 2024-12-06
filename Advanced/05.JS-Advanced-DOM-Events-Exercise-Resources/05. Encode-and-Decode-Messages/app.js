4fd53cd1591938386538ec76b8402b96d94a745e
Svetozar Stoev <zarkoto23@gmail.com>
2024-12-06 15:47:06 +0200

    new solvings

--- Advanced/05.JS-Advanced-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
+++ Advanced/05.JS-Advanced-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,30 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  const btns = document.querySelectorAll("button");
+  const areas = document.querySelectorAll("textarea");
+
+  const btnEncode = btns[0];
+  const btnDecode = btns[1];
+
+  const inputTxt = areas[0];
+  const outputTxt = areas[1];
+
+  btnEncode.addEventListener("click", (e) => {
+    const text = inputTxt.value
+      .split("")
+      .map((el) => String.fromCharCode(el.charCodeAt() + 1))
+      .join("");
+
+    outputTxt.value = text;
+    
+    inputTxt.value = "";
+
+  });
+
+  btnDecode.addEventListener("click", (e) => {
+    outputTxt.value = outputTxt.value
+      .split("")
+      .map((el) => String.fromCharCode(el.charCodeAt() - 1))
+      .join("");
+    inputTxt.focus();
+  });
 }
