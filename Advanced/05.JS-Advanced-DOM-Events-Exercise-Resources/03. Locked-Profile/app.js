c0276fa9ffd0aef15bf0d329b60b90ca17ddf706
Svetozar Stoev <zarkoto23@gmail.com>
2024-12-06 15:09:02 +0200

    new solvings

--- Advanced/05.JS-Advanced-DOM-Events-Exercise-Resources/03. Locked-Profile/app.js
+++ Advanced/05.JS-Advanced-DOM-Events-Exercise-Resources/03. Locked-Profile/app.js
@@ -1,3 +1,22 @@
 function lockedProfile() {
-    console.log('TODO...')
+  const profiles = document.querySelectorAll(".profile");
+
+  profiles.forEach((profile) => {
+    const lockRadio = profile.querySelector('input[value="lock"]');
+    const unlockRadio = profile.querySelector('input[value="unlock"]');
+    const button = profile.querySelector("button");
+    const hidenFrends = profile.querySelector('div[id*="HiddenFields"]');
+
+    button.addEventListener("click", () => {
+      if (unlockRadio.checked) {
+        if (button.textContent == "Show more") {
+          hidenFrends.style.display = "block";
+          button.textContent = "Hide it";
+        } else {
+          hidenFrends.style.display = "none";
+          button.textContent = "Show more";
+        }
+      }
+    });
+  });
 }
--- Advanced/05.JS-Advanced-DOM-Events-Exercise-Resources/04. Fill-Dropdown/dropdown.js
+++ Advanced/05.JS-Advanced-DOM-Events-Exercise-Resources/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,19 @@
 function addItem() {
-    console.log('TODO:...');
+  const menuEl = document.querySelector("#menu");
+
+  const itemTxt = document.querySelector("#newItemText");
+  const itemValue = document.querySelector("#newItemValue");
+
+  if (itemTxt.value == "" || itemValue.value == "") return;
+
+  const newEl = document.createElement("option");
+
+  newEl.textContent = itemTxt.value;
+  newEl.value = itemValue.value;
+
+  menuEl.appendChild(newEl);
+
+  itemTxt.value = "";
+  itemValue.value = "";
+  itemTxt.focus();
 }
