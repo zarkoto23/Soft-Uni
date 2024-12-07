3cd7de40a98859883eb8eb97719a11cead94ee07
Svetozar Stoev <zarkoto23@gmail.com>
2024-12-07 22:56:54 +0200

    furniture new solve

--- Advanced/05.JS-Advanced-DOM-Events-Exercise-Resources/06. Furniture/solution.js
+++ Advanced/05.JS-Advanced-DOM-Events-Exercise-Resources/06. Furniture/solution.js
@@ -1,66 +1,62 @@
 function solve() {
-  const generateButton = document.querySelectorAll("button")[0];
-  const buyButton = document.querySelectorAll("button")[1];
-
-
-  generateButton.addEventListener("click", (e) => {
-  const inputTextArea = document.querySelectorAll("textarea")[0];
-
+  const btnGen = document.querySelector("button");
+  btnGen.addEventListener("click", (e) => {
+    const inputEl = document.querySelector("textarea");
+    const data = JSON.parse(inputEl.value);
     const productListEl = document.querySelector("table.table tbody");
-    const productData = JSON.parse(inputTextArea.value);
 
-    productData.forEach((item) => {
+    data.forEach((item) => {
       const product = document.createElement("tr");
 
-      const imageCell = document.createElement("td");
-      const image = document.createElement("img");
-      image.setAttribute("src", item.img);
-      imageCell.appendChild(image);
+      const productImageCell = document.createElement("td");
+      const productImage = document.createElement("img");
+      productImage.setAttribute("src", item.img);
+      productImageCell.appendChild(productImage);
 
-      const name = document.createElement("td");
-      name.textContent = item.name;
+      const productNameCell = document.createElement("td");
+      productNameCell.textContent = item.name;
 
-      const price = document.createElement("td");
-      price.textContent = (item.price);
-      const decFactor = document.createElement("td");
-      decFactor.textContent = (item.decFactor);
-      const mark = document.createElement("td");
-      const markChekbox = document.createElement("input");
-      markChekbox.setAttribute("type", "checkbox");
-      mark.appendChild(markChekbox);
+      const productPriceCell = document.createElement("td");
+      productPriceCell.textContent = item.price;
 
-      product.append(imageCell, name, price, decFactor, mark);
-      productListEl.appendChild(product);
+      const productDecFactorCell = document.createElement("td");
+      productDecFactorCell.textContent = item.decFactor;
+
+      const productMarkCell = document.createElement("td");
+      const productMarkCheckBox = document.createElement("input");
+      productMarkCheckBox.setAttribute("type", "checkbox");
+      productMarkCell.appendChild(productMarkCheckBox);
+
+      product.append(
+        productImageCell,
+        productNameCell,
+        productPriceCell,
+        productDecFactorCell,
+        productMarkCell
+      );
+
+      productListEl.appendChild(product)
     });
   });
 
-  buyButton.addEventListener("click", (e) => {
-  const outputTextArea = document.querySelectorAll("textarea")[1];
-  outputTextArea.value=''
+  const shopBtnEl=document.querySelectorAll('button')[1]
+  shopBtnEl.addEventListener('click', (e)=>{
 
-    const data = [
-      ...document.querySelectorAll(".table tbody tr:has(input:checked"),
-    ].map((i) => ({
-      name: i.childNodes[1].textContent,
-      price: Number(i.childNodes[2].textContent),
-      decFactor: Number(i.childNodes[3].textContent),
-    }));
+    const outputEl=document.querySelectorAll('textarea')[1]
 
-    if (data.length < 1) return;
-    outputTextArea.disabled=false
+    const data=[...document.querySelectorAll('.table tbody tr:has(input:checked)')].map(i=>({
+      name:i.childNodes[1].textContent,
+      price:Number(i.childNodes[2].textContent),
+      decFactor:Number(i.childNodes[3].textContent)
 
-    let output = `Bought furniture: ${data.map((e) => e.name).join(", ")}\n`;
-    output += `Total price: ${Number(
-      data.reduce((acc, el) => {
-        return acc + el.price;
-      }, 0)
-    ).toFixed(2)}\n`;
-
-    output += `Average decoration factor: ${
-      (data.reduce((acc, el) => acc + el.decFactor, 0) / data.length)
-    }`;
+    }))
     
+    let output=`Bought furniture: ${data.map(i=>i.name).join(', ')}\n`
+    output+=`Total price: ${Number(data.reduce((acc, el)=>{return acc+el.price},0).toFixed(2))}\n`
+    output+=`Average decoration factor: ${data.reduce((acc, el)=> {return acc+el.decFactor},0)/data.length}`
 
-    outputTextArea.value = output;
-  });
+    outputEl.value=output
+
+  })
+
 }
