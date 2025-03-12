e1220e536eded8468772c4242f83de8b5d1cd65b
Svetozar Stoev <zarkoto23@gmail.com>
2025-03-12 17:02:49 +0200

    printDeckOfCards

---
+++ Advanced/unit test, error handling lab&exersices/deckOfCards.js
@@ -0,0 +1,49 @@
+function printDeckOfCards(cards) {
+
+  
+  const validFaces = new Set([
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A",
+  ]);
+
+  const suits = {
+    C: "\u2663",
+    D: "\u2666",
+    H: "\u2665",
+    S: "\u2660",
+  };
+  let result = new Set()
+  let card, color
+
+  for(const str of cards){
+    if(str.length>1){
+      card=str.slice(0,-1)
+      color=str.slice(-1)
+    }else{
+      card=str[0]
+      color=str[1]
+    }
+  
+  if(!validFaces.has(card)){
+    throw new Error(`Invalid card: ${str}`)
+  }
+  result.add(`${card}${suits[color]}`)
+
+}
+
+  console.log([...result].join(' '))
+
+}
+
+printDeckOfCards(['5S', '3D', 'QD'])
