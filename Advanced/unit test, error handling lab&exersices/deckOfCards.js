6d993d27600c77e750b0955dab19c09600311d0e
Svetozar Stoev <zarkoto23@gmail.com>
2025-03-12 17:17:46 +0200

    deckOfCardsFixed

--- Advanced/unit test, error handling lab&exersices/deckOfCards.js
+++ Advanced/unit test, error handling lab&exersices/deckOfCards.js
@@ -1,7 +1,5 @@
 function printDeckOfCards(cards) {
-
-  
-  const validFaces = new Set([
+  const validFaces = [
     "2",
     "3",
     "4",
@@ -15,7 +13,7 @@ function printDeckOfCards(cards) {
     "Q",
     "K",
     "A",
-  ]);
+  ];
 
   const suits = {
     C: "\u2663",
@@ -23,27 +21,26 @@ function printDeckOfCards(cards) {
     H: "\u2665",
     S: "\u2660",
   };
-  let result = new Set()
-  let card, color
+  let result = [];
+  let card, color;
 
-  for(const str of cards){
-    if(str.length>1){
-      card=str.slice(0,-1)
-      color=str.slice(-1)
-    }else{
-      card=str[0]
-      color=str[1]
+  for (const str of cards) {
+    if (str.length > 1) {
+      card = str.slice(0, -1);
+      color = str.slice(-1);
+    } else {
+      card = str[0];
+      color = str[1];
     }
-  
-  if(!validFaces.has(card)){
-    throw new Error(`Invalid card: ${str}`)
+
+    if (!validFaces.includes(card) || !suits.hasOwnProperty(color)) {
+      console.log(`Invalid card: ${str}`);
+      return;
+    }
+    result.push(`${card}${suits[color]}`);
   }
-  result.add(`${card}${suits[color]}`)
 
+  console.log([...result].join(" "));
 }
 
-  console.log([...result].join(' '))
-
-}
-
-printDeckOfCards(['5S', '3D', 'QD'])
+printDeckOfCards(["5S", "3J", "10D", "QD"]);
