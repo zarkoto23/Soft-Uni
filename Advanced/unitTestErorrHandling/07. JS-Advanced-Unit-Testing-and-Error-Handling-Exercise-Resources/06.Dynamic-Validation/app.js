96e049cdd43c19cf88a98e1df7015fddbb9be227
Svetozar Stoev <zarkoto23@gmail.com>
2025-03-13 17:14:08 +0200

    dynamicValidation solved

--- Advanced/unitTestErorrHandling/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/06.Dynamic-Validation/app.js
+++ Advanced/unitTestErorrHandling/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/06.Dynamic-Validation/app.js
@@ -1,3 +1,8 @@
 function validate() {
-    // TODO
+
+    const pattern=/^.*@.*\..*$/g
+   const emailEl=document.querySelector('#email')
+   emailEl.addEventListener('change',(e)=>{
+    e.target.className=(!pattern.test(e.target.value))  ? 'error':''
+   })
 }
