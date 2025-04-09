7c0926b9f58652c99b0a1a6ad6911c21bd5dd8d9
Svetozar Stoev <zarkoto23@gmail.com>
2025-04-09 18:48:18 +0300

    promises

--- Applications/AsynchronousProgramming/demo.js
+++ Applications/AsynchronousProgramming/demo.js
@@ -2,7 +2,7 @@ function proposal() {
   const promise = new Promise((resolve, reject) => {
     console.log("marry me ?");
     setTimeout(() => {
-      if (Math.random() < 0.5) {
+      if (Math.random() <0.5) {
         resolve("just married");
       } else {
         reject("bad proposal");
@@ -19,3 +19,17 @@ proposal()
   .catch((err) => {
     console.log("problem catch");
   });
+
+const proposalOne = proposal();
+const proposalTwo = proposal();
+const proposalThree = proposal();
+const proposalFour = proposal();
+
+const grup = Promise.all([
+  proposalOne,
+  proposalTwo,
+  proposalThree,
+  proposalFour,
+]).then((result) => console.log(result))
+.catch((errr) => console.log(errr))
+.finally(()=>console.log('asdasdas'))
