8a918014b443bad033101e711ba021302ab713e8
Svetozar Stoev <zarkoto23@gmail.com>
2025-04-09 19:10:17 +0300

    chaining

--- Applications/AsynchronousProgramming/01.XHR/scripts/app.js
+++ Applications/AsynchronousProgramming/01.XHR/scripts/app.js
@@ -1,12 +1,18 @@
-function loadRepos() {
-  const url = "https:api.github.com/users/testnakov/repos";
-  const httpReq = new XMLHttpRequest();
+// function loadRepos() {
+//   const url = "https:api.github.com/users/testnakov/repos";
+//   const httpReq = new XMLHttpRequest();
 
-  httpReq.addEventListener("readystatechange", function () {
-    if (httpReq.readyState == 4 && httpReq.status == 200) {
-      (document.getElementById("res").textContent = httpReq), this.responseText;
-    }
-  });
-  httpReq.open("GET", url);
-  httpReq.send();
+//   httpReq.addEventListener("readystatechange", function () {
+//     if (httpReq.readyState == 4 && httpReq.status == 200) {
+//       (document.getElementById("res").textContent = httpReq), this.responseText;
+//     }
+//   });
+//   httpReq.open("GET", url);
+//   httpReq.send();
+// }
+
+function fetchRepos() {
+  const url = "https:api.github.com/users/testnakov/repos"
+
+
 }
---
+++ Applications/AsynchronousProgramming/fetch-repos.js
@@ -0,0 +1,7 @@
+
+  const url = "https:api.github.com/users/testnakov/repos"
+
+
+  fetch(url)
+  .then(res=>console.log(res))
+  
