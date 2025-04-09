6a0a6839adbcb2b7ad840973b7aa86308705ba92
Svetozar Stoev <zarkoto23@gmail.com>
2025-04-09 17:24:30 +0300

    XHR task solved

--- Applications/AsynchronousProgramming/01.XHR/scripts/app.js
+++ Applications/AsynchronousProgramming/01.XHR/scripts/app.js
@@ -1,3 +1,12 @@
 function loadRepos() {
-   console.log("TODO...");
+  const url = "https:api.github.com/users/testnakov/repos";
+  const httpReq = new XMLHttpRequest();
+
+  httpReq.addEventListener("readystatechange", function () {
+    if (httpReq.readyState == 4 && httpReq.status == 200) {
+      (document.getElementById("res").textContent = httpReq), this.responseText;
+    }
+  });
+  httpReq.open("GET", url);
+  httpReq.send();
 }
