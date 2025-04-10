a90f09e8222c362bae90c3dcbd4a445a93375593
Svetozar Stoev <zarkoto23@gmail.com>
2025-04-10 21:33:43 +0300

    taskTwo solved

--- Applications/exersiceAsynchronousProgramming/02.Bus-Schedule/app.js
+++ Applications/exersiceAsynchronousProgramming/02.Bus-Schedule/app.js
@@ -1,11 +1,32 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+  const label=document.querySelector('.info')
+  const departBtn=document.getElementById('depart')
+  const arriveBtn=document.getElementById('arrive')
+
+  let stop={
+    next:'depot',
+  }
+  
+  async function depart() {
+    departBtn.disabled=true
+    const url=`http://localhost:3030/jsonstore/bus/schedule/${stop.next}`
+    const res=await fetch(url)
+
+    if(res.status!==200){
+      label.textContent='Error---'
+      departBtn.disabled=true
+      arriveBtn.disabled=true
+    }
+    stop=await res.json()
+    label.textContent=`Next Stop: ${stop.name}`
+    arriveBtn.disabled=false
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+      label.textContent=`Arriving at: ${stop.name}`
+      arriveBtn.disabled=true
+      departBtn.disabled=false
     }
 
     return {
@@ -15,3 +36,7 @@ function solve() {
 }
 
 let result = solve();
+
+
+
+//http://localhost:3030/jsonstore/bus/schedule/:id 
