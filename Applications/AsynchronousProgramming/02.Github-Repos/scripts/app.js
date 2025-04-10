d21e35c95a951942f383459d16b6ff39585aa304
Svetozar Stoev <zarkoto23@gmail.com>
2025-04-10 19:07:54 +0300

    github repos solve

--- Applications/AsynchronousProgramming/02.Github-Repos/scripts/app.js
+++ Applications/AsynchronousProgramming/02.Github-Repos/scripts/app.js
@@ -1,3 +1,25 @@
 function loadRepos() {
-	
-}
+  const repostUl=document.getElementById('repos')
+	const username=document.getElementById('username').value
+  const url=`https://api.github.com/users/${username}/repos`
+
+  fetch(url)
+    .then(res=>res.json())
+    .then(repos=>{
+
+      repostUl.innerHTML=''
+      
+      
+      repos.forEach(repo => {
+      
+      const aEl=document.createElement('a')
+      aEl.href=repo.html_url
+      aEl.textContent=repo.full_name
+
+      const liEl=document.createElement('li')
+      liEl.appendChild(aEl)
+      repostUl.appendChild(liEl)
+    })})
+    .catch(err=>console.log(err))
+  }
+
