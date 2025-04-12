3ea39b26db890d589fffdf92cda2e90a260f12fb
Svetozar Stoev <zarkoto23@gmail.com>
2025-04-12 20:19:06 +0300

    busStop new solving

--- Applications/exersiceAsynchronousProgramming/01.Bus-Stop/app.js
+++ Applications/exersiceAsynchronousProgramming/01.Bus-Stop/app.js
@@ -1,5 +1,24 @@
 async function getInfo() {
+  const stopId = document.getElementById("stopId").value;
+  const infoUrl = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+  const stopNameEl = document.getElementById("stopName");
 
+  const busesEl = document.getElementById("buses");
 
-  
+  try {
+    busesEl.replaceChildren();
+    const res = await fetch(infoUrl);
+
+    const data = await res.json();
+
+    stopNameEl.textContent = data.name;
+
+    Object.entries(data.buses).forEach((element) => {
+      const li = document.createElement("li");
+      li.textContent = `Bus ${element[0]} arrives in ${element[1]} minutes`;
+      busesEl.appendChild(li);
+    });
+  } catch (err) {
+    stopNameEl.textContent = "Error";
+  }
 }
--- Applications/exersiceAsynchronousProgramming/01.Bus-Stop/app2.js
+++ Applications/exersiceAsynchronousProgramming/01.Bus-Stop/app2.js
@@ -33,3 +33,4 @@ async function getInfo() {
 
 
 }
+
