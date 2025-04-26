a44feda9370e3964fdfae5285e86bad432e5266d
Svetozar Stoev <zarkoto23@gmail.com>
2025-04-26 18:12:18 +0300

    book-library solved

--- Applications/RemoteDataAuthenticationsExersice/04.Book-library/app.js
+++ Applications/RemoteDataAuthenticationsExersice/04.Book-library/app.js
@@ -30,13 +30,13 @@ document.addEventListener("DOMContentLoaded", () => {
       const editBtn = document.createElement("button");
       editBtn.textContent = "Edit";
       editBtn.setAttribute("data-id", id);
-      // editBtn.addEventListener("click", handleEdit);
+      editBtn.addEventListener("click", handleEdit);
       row.appendChild(editBtn);
 
       const delBtn = document.createElement("button");
       delBtn.textContent = "Delete";
       delBtn.setAttribute("data-id", id);
-      // editBtn.addEventListener("click", handleDelete);
+      delBtn.addEventListener("click", handleDelete);
       row.appendChild(delBtn);
 
       row.appendChild(actionCell);
@@ -46,20 +46,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
   async function handleFormSubmit(e) {
     e.preventDefault();
+    console.log("click");
+
     const formData = new FormData(bookForm);
 
     const title = formData.get("title");
     const author = formData.get("author");
     const bookId = bookForm.getAttribute("data-id");
 
+    if (title === "" || author === "") {
+      return;
+    }
+
     const book = { title, author };
 
     try {
       let response;
       if (bookId) {
-        // response=await fetch(`${url}/${bookId}`,{
-        //   method:'POST',
-        // })
+        response = await fetch(`${url}/${bookId}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(book),
+        });
+
+        bookForm.removeAttribute("data-id");
+        bookForm.querySelector("h3").textContent = "FORM";
+        bookForm.querySelector('input[name="title"]').value = "";
+        bookForm.querySelector('input[name="author"]').value = "";
+        loadBooks();
       } else {
         response = await fetch(url, {
           method: "POST",
@@ -75,5 +91,30 @@ document.addEventListener("DOMContentLoaded", () => {
     } catch (error) {
       console.error(error);
     }
+  }
+
+  async function handleDelete(e) {
+    const bookId = e.target.getAttribute("data-id");
+
+    await fetch(`${url}/${bookId}`, {
+      method: "DELETE",
+    });
+
+    e.target.closest("tr").remove();
+  }
+
+  async function handleEdit(e) {
+    const bookId = e.target.getAttribute("data-id");
+    const row = e.target.closest("tr");
+    const cells = row.querySelectorAll("td");
+
+    const title = cells[0].textContent;
+    const author = cells[1].textContent;
+
+    bookForm.querySelector('[name="title"]').value = title;
+    bookForm.querySelector('[name="author"]').value = author;
+
+    bookForm.setAttribute("data-id", bookId);
+    bookForm.querySelector("h3").textContent = "EditFORM";
   }
 });
