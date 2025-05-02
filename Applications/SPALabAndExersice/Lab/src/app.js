12651a0353cb6f2200e2de5c61d243dcd2336caf
Svetozar Stoev <zarkoto23@gmail.com>
2025-05-02 18:05:53 +0300

    refactoring

--- Applications/SPALabAndExersice/Lab/src/app.js
+++ Applications/SPALabAndExersice/Lab/src/app.js
@@ -1,24 +1,42 @@
 import { startCatalog } from "./views/catalog.js";
 import { showView } from "./views/nav.js";
 
-document.getElementById("home-nav").addEventListener("click", (event) => {
-  event.preventDefault();
-  showView('home');
-});
+const views={
+  'home-nav':['home'],
+  'catalog-nav':['catalog', startCatalog],
+  'catalog-link':['catalog',startCatalog],
+}
 
-document.getElementById("catalog-nav").addEventListener("click", (event) => {
-  event.preventDefault();
-  showView('catalog',startCatalog);
-});
+for(let linkId in views){
+  document.getElementById(linkId).addEventListener('click', (event)=>{
+    event.preventDefault()
 
-document.getElementById("catalog-link").addEventListener("click", (event) => {
-  event.preventDefault();
-  
-  showView('catalog', startCatalog)
+    const [sectionId, callback]=views[linkId]
 
-
-});
+    showView(sectionId, callback);
+    
+  })
+}
 showView('home')
+
+
+// document.getElementById("home-nav").addEventListener("click", (event) => {
+//   event.preventDefault();
+//   showView('home');
+// });
+
+// document.getElementById("catalog-nav").addEventListener("click", (event) => {
+//   event.preventDefault();
+//   showView('catalog',startCatalog);
+// });
+
+// document.getElementById("catalog-link").addEventListener("click", (event) => {
+//   event.preventDefault();
+  
+//   showView('catalog', startCatalog)
+
+
+// });
 
 
 
