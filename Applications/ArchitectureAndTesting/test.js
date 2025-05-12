604dca5f51c4c8e66b4695160ff655032f8feca1
Svetozar Stoev <zarkoto23@gmail.com>
2025-05-12 13:51:46 +0300

    test init

---
+++ Applications/ArchitectureAndTesting/test.js
@@ -0,0 +1,9 @@
+const { chromium } = require("playwright-chromium");
+
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto("https://softuni.bg/trainings/resources/video/101698/video-08-july-2024-viktor-kostadinov-js-applications-june-2024/4544");
+  await page.screenshot({ path: "screenshot.png" });
+  await browser.close();
+})();
