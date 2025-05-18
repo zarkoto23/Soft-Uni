46ab47b8c9c718e0250ed5a6b3d20941c6156de7
Svetozar Stoev <zarkoto23@gmail.com>
2025-05-18 21:10:01 +0300

    console git test

--- Applications/rendering/01.Contacts/app.js
+++ Applications/rendering/01.Contacts/app.js
@@ -7,27 +7,29 @@ const cardTemplate = (data) => html` <div class="contact card">
   </div>
   <div class="info">
     <h2>Name: ${data.name}</h2>
-    <button @click=${()=>toggleDetails(data)} class="detailsBtn">Details</button>
-     ${
-      data.details
-      ? html`
-    <div class="details" id=${data.id}>
-      <p>Phone number: ${data.phoneNumber}</p>
-      <p>Email: ${data.email}</p>
-    </div>` 
-    : null 
-     }
+    <button @click=${toggleDetails.bind(data)} class="detailsBtn">
+      Details
+    </button>
+    ${data.details
+      ? html` <div class="details" id=${data.id}>
+          <p>Phone number: ${data.phoneNumber}</p>
+          <p>Email: ${data.email}</p>
+        </div>`
+      : null}
   </div>
-</div>`
+</div>`;
 
 const container = document.getElementById("contacts");
 
-update()
-function update(){
-render(contacts.map(cardTemplate), container);
+update();
+function update() {
+  render(contacts.map(cardTemplate), container);
 }
 
-function toggleDetails(contact){
-  contact.details=!contact.details
-  update()
+function toggleDetails() {
+  for (const contact of contacts) {
+    contact.details = contact == this&& !this.details
+  }
+
+  update();
 }
