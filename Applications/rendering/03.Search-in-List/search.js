16e09d98d2ca6642852488e02d4264fcd9d9ad3c
Svetozar Stoev <zarkoto23@gmail.com>
2025-05-20 00:10:27 +0300

    search list completed

--- Applications/rendering/03.Search-in-List/search.js
+++ Applications/rendering/03.Search-in-List/search.js
@@ -1,3 +1,32 @@
-function search() {
-   // TODO
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+import { towns } from "./towns.js";
+
+const searchBtn = document
+  .querySelector("button")
+  .addEventListener("click", onClick);
+const townDiv = document.getElementById("towns");
+const result=document.getElementById('result')
+const townsTemplate = html` <ul>
+  ${towns.map((t) => html`<li>${t}</li>`)}
+</ul>`;
+
+render(townsTemplate, townDiv);
+
+
+function onClick() {
+let counter=0
+
+
+  const inputText = document.getElementById("searchText");
+  const townElLi = [...document.querySelectorAll("#towns li")];
+  townElLi.forEach((town) => {
+    town.classList.remove("active");
+    if (town.textContent.includes(inputText.value)&&inputText.value!='') {
+      town.classList.add("active");
+      counter++
+    }
+  });
+    inputText.value = "";
+    result.textContent=`${counter} matches found`
+
 }
