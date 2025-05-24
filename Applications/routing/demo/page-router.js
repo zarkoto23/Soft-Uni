3fdc1f1fe945576161c156fd1cfef6ab4c45c441
Svetozar Stoev <zarkoto23@gmail.com>
2025-05-24 23:14:21 +0300

    routing autopats

--- Applications/routing/demo/page-router.js
+++ Applications/routing/demo/page-router.js
@@ -1,7 +1,35 @@
-import page from "../node_modules/page/page.mjs
+import page from "https://unpkg.com/page/page.mjs";
 
-function showView(id) {
+document.querySelector("button").addEventListener("click", onLogin);
+
+function onLogin() {
+  page.redirect("/catalog");
+}
+
+// page((ctx, next)=>{console.log('userNav'); next()})
+page('/index.html', '/')
+page('/home', '/')
+page('/', () => showView("home"));
+page("/catalog", () => showView("catalog"));
+page("/about", () => showView("about"));
+page("/login", () => showView("login"));
+
+page("/details/:id", (context) => showView("details",context));
+
+
+
+
+page.start();
+
+function showView(id,event) {
   document.querySelectorAll("section").forEach((s) => {
     s.style.display = s.id == id ? "block" : "none";
   });
+  
+  const productId=event?.params.id
+  
+
+  if(productId){
+    document.querySelector('p').textContent='product details'+productId
+  }
 }
