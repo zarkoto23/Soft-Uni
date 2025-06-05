20569623af7f257db09072afb50263dc9c5bab89
Svetozar Stoev <zarkoto23@gmail.com>
2025-06-05 11:18:53 +0300

    init api.js

--- Applications/modularApp/01.Team-Manager/src/constants.js
+++ Applications/modularApp/01.Team-Manager/src/constants.js
@@ -1,6 +1,5 @@
-const baseUrl='http://localhost:3030/'
+const baseUrl = "http://localhost:3030/";
 
-const baseUsersUrl=`${baseUrl}/users`
-const baseTeamsUrl=`${baseUrl}/data/teams`
-const baseMembersUrl=`${baseUrl}/data/members`
-
+const baseUsersUrl = `${baseUrl}/users`;
+const baseTeamsUrl = `${baseUrl}/data/teams`;
+const baseMembersUrl = `${baseUrl}/data/members`;
