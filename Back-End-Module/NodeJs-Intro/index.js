ff9139b523647fc32a3b2166f0a2b5e675695fd5
Svetozar Stoev <zarkoto23@gmail.com>
2025-06-23 23:34:41 +0300

    demo last commit

--- Back-End-Module/NodeJs-Intro/index.js
+++ Back-End-Module/NodeJs-Intro/index.js
@@ -1,9 +1,33 @@
+
+
 const http=require('http')
+const html=`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>SHIBAISE</h1>
+</body>
+</html>`
 
-const server=http.createServer(()=>{
 
-    console.log('request recieved');
+const server=http.createServer((request, response)=>{
+    console.log(request.method);
+    console.log(request.url);
+    console.log(request.headers);
     
+    
+   if(request.url=='/favicon.ico'){
+    response.statusCode=404
+    response.end()
+   }else{
+    response.writeHead(200,['content-type', 'text/html'])
+    response.write(html)
+    response.end()
+   }
 })
 
 server.listen(3000)
