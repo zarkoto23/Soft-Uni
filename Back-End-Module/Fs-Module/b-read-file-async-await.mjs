8f2546db38d52422c1221627f991981b65ed21a2
Svetozar Stoev <zarkoto23@gmail.com>
2025-06-28 13:31:04 +0300

    async await more examples

--- Back-End-Module/Fs-Module/b-read-file-async-await.mjs
+++ Back-End-Module/Fs-Module/b-read-file-async-await.mjs
@@ -2,9 +2,17 @@ import fs from "fs/promises";
 
 //async file reading with promises (async/await syntax)
 
-const loremntext=await fs.readFile("./input.html", { encoding: "utf-8" })
+try{const loremntext=await fs.readFile("./input.html", { encoding: "utf-8" })
 
 console.log(loremntext);
-
-
 console.log("\n                                   READED!");
+
+
+
+}catch(err){
+  console.log(err.message);
+  
+}
+
+
+
