5de3702f5793e10d74da6f4b0b45e5ff85d81c8c
Svetozar Stoev <zarkoto23@gmail.com>
2025-06-28 13:26:34 +0300

    promise then sintax async file reading

---
+++ Back-End-Module/Fs-Module/b-read-file-async-promises.mjs
@@ -0,0 +1,18 @@
+import fs from 'fs/promises'
+
+
+//async file reading with promises (promise then syntax)
+
+
+fs.readFile('./input.html',{encoding:'utf-8'})
+.then(loremText=>{
+    console.log(loremText);
+    
+}).catch(err=>{
+    console.log(err.message);
+    
+})
+
+
+console.log("\n                                   READED!");
+
