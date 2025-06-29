57d8402d7cc915daa2ffa6471224e5df69774e76
Svetozar Stoev <zarkoto23@gmail.com>
2025-06-29 12:41:22 +0300

    fix: correct spelling errors in index.js and update express dependency version in package.json and package-lock.json

--- Back-End-Module/Express-And-Templating/Express/index.js
+++ Back-End-Module/Express-And-Templating/Express/index.js
@@ -1,6 +1,6 @@
-import exepress from "express";
+import express from "express";
 
-const app = exepress();
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("<h1>helloW from sve</h1>");
@@ -9,5 +9,10 @@ app.get("/", (req, res) => {
 app.get("/cats", (req, res) => {
   res.send("<h1>Meow from cats</h1>");
 });
+
+app.all('*',(req, res)=> {
+    res.send('<h1>Not found _|_</h1>')
+})
+
 
 app.listen(3000, () => console.log("hellow from Xpress"));
