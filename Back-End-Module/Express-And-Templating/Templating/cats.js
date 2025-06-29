d0109871bd9063c69a29ac03c33e37015ff15426
Svetozar Stoev <zarkoto23@gmail.com>
2025-06-29 19:00:58 +0300

    feat: implement cat management features with handlebars views and routing

---
+++ Back-End-Module/Express-And-Templating/Templating/cats.js
@@ -0,0 +1,38 @@
+export default  [
+  {
+    id: 1,
+    isStar:true,
+    name: "Mittens",
+    description: "Малка сива котка, която обича да се катери по завесите.",
+    breed: "British Shorthair",
+    imageUrl: "https://i.pim/736x/e3/24/f7/e324f790cfe0a51d76f98356475cc408.jpg"
+  },
+  {
+    id: 2,
+    name: "Luna",
+    description: "Много игрива и обича да гони светлинки.",
+    breed: "Siamese",
+    imageUrl: "https://example.com/images/luna.jpg"
+  },
+  {
+    id: 3,
+    name: "Simba",
+    description: "Обича да спи по цял ден и да хапва лакомства.",
+    breed: "Maine Coon",
+    imageUrl: "https://example.com/images/simba.jpg"
+  },
+  {
+    id: 4,
+    name: "Cleo",
+    description: "Много мила и дружелюбна котка, подходяща за деца.",
+    breed: "Persian",
+    imageUrl: "https://example.com/images/cleo.jpg"
+  },
+  {
+    id: 5,
+    name: "Shadow",
+    description: "Потаен и мистериозен, но се привързва лесно.",
+    breed: "Bombay",
+    imageUrl: "https://example.com/images/shadow.jpg"
+  }
+];
--- Back-End-Module/Express-And-Templating/Templating/index.js
+++ Back-End-Module/Express-And-Templating/Templating/index.js
@@ -1,12 +1,28 @@
 import express from "express";
 import handlebars from "express-handlebars";
+import cats from "./cats.js";
 
 const app = express();
 
-app.engine("handlebars", handlebars.engine());
+//add handlebars engine to xpress engines
+app.engine("hbs", handlebars.engine({
+    extname:'hbs'
+}));
+
+//set default engine
+app.set("view engine", "hbs");
+//
 
 app.get("/", (req, res) => {
-  res.send("it WORK");
+  res.render("home",{cats});
 });
+
+app.get('/add-cat',(req, res)=>{
+    res.render('addCat',{layout:'secondary'})
+})
+
+app.get('/add-breed',(req, res)=>{
+    res.render('addBreed')
+})
 
 app.listen(3000, () => console.log("listen on http://localhost3000"));
