0bd59bab738ab18e8022a86416162414b117358e
Svetozar Stoev <zarkoto23@gmail.com>
2025-06-30 13:28:38 +0300

    setup handlebars

--- Back-End-Module/Workshop-Movie-Magic/src/index.js
+++ Back-End-Module/Workshop-Movie-Magic/src/index.js
@@ -1,10 +1,27 @@
 import express from 'express'
+import handlebars from 'express-handlebars'
 
 const app=express()
+
+app.engine('hbs',handlebars.engine({
+    extname:'hbs',
+}))
+app.set('view engine','hbs')
+app.set('views',"./src/views")
+
+
 
 app.get('/',(req, res)=>{
     res.send('itWorks')
 })
+
+
+
+
+
+
+
+
 
 
 app.listen(3000,() =>{console.log('listen on http://localhost:3000')});
