e016a83ddcca9c74218cf7e5ac0c021596e3086d
Svetozar Stoev <zarkoto23@gmail.com>
2025-06-30 18:04:06 +0300

    fix: update movie IDs and image URLs, enhance details link in home view

--- Back-End-Module/Workshop-Movie-Magic/src/movies.js
+++ Back-End-Module/Workshop-Movie-Magic/src/movies.js
@@ -1,32 +1,37 @@
 export default [
   {
+    id: "1",
     title: "Inception",
     description: "Един крадец, който прониква в сънища, получава задачата да имплантира идея в съзнанието на цел.",
     genre: "Фантастика, Трилър",
-    imageUrl: "https://image.tmdb.org/t/p/w500/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg"
+    imageUrl: "https://m.media-amazon.com/images/M/MV5BMjExMjkwNTQ0Nl5BMl5BanBnXkFtZTcwNTY0OTk1Mw@@._V1_.jpg"
   },
   {
+    id: "2",
     title: "The Shawshank Redemption",
     description: "Несправедливо осъден мъж изгражда приятелство и надежда в затвора Шоушенк.",
     genre: "Драма",
     imageUrl: "https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg"
   },
   {
+    id: "3",
     title: "The Dark Knight",
     description: "Батман се изправя срещу Жокера, за да спаси Готъм от хаос.",
     genre: "Екшън, Криминален",
     imageUrl: "https://image.tmdb.org/t/p/w500/1hRoyzDtpgMU7Dz4JF22RANzQO7.jpg"
   },
   {
+    id: "4",
     title: "Interstellar",
     description: "Екип от изследователи преминава през черна дупка в търсене на нов дом за човечеството.",
     genre: "Научна фантастика, Приключенски",
     imageUrl: "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg"
   },
   {
+    id: "5",
     title: "Pulp Fiction",
     description: "Преплетени истории на престъпници, боксори и мистериозен куфар в Лос Анджелис.",
     genre: "Криминален, Драма",
-    imageUrl: "https://image.tmdb.org/t/p/w500/dM2w364MScsjFf8pfMbaWUcWrR.jpg"
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg"
   }
 ];
