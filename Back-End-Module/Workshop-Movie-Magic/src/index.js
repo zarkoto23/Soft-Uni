4db0827bb0b468d6a91118cf5569af87fff1c459
Svetozar Stoev <zarkoto23@gmail.com>
2025-06-30 13:46:57 +0300

    Refactor home view to use Handlebars and remove old HTML file

--- Back-End-Module/Workshop-Movie-Magic/src/index.js
+++ Back-End-Module/Workshop-Movie-Magic/src/index.js
@@ -2,26 +2,24 @@ import express from 'express'
 import handlebars from 'express-handlebars'
 
 const app=express()
-
+//
 app.engine('hbs',handlebars.engine({
     extname:'hbs',
 }))
 app.set('view engine','hbs')
 app.set('views',"./src/views")
 
-
-app.use(express.static('src/public'))
-
-
+app.use('/static',express.static('src/public'))
+//
 
 
 
 
+//
 app.get('/',(req, res)=>{
-    res.send('itWorks')
+    res.render('home',{layout:false})
 })
-
-
+ //
 
 
 
