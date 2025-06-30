cfee471895bc4e3e8dea83e2c220319d53f3d8ef
Svetozar Stoev <zarkoto23@gmail.com>
2025-06-30 13:20:15 +0300

    install and setup express

--- Back-End-Module/Workshop-Movie-Magic/src/index.js
+++ Back-End-Module/Workshop-Movie-Magic/src/index.js
@@ -1,1 +1,10 @@
-console.log('itWork')
+import express from 'express'
+
+const app=express()
+
+app.get('/',(req, res)=>{
+    res.send('itWorks')
+})
+
+
+app.listen(3000,() =>{console.log('listen on http://localhost:3000')});
