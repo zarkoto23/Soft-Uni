1188a165050b9e60aebcddc5ff8e7b04ef1f2a83
Svetozar Stoev <zarkoto23@gmail.com>
2025-07-07 11:32:34 +0300

    feat: add mongoose for MongoDB connection and update package.json

--- Back-End-Module/Workshop-Movie-Magic/src/index.js
+++ Back-End-Module/Workshop-Movie-Magic/src/index.js
@@ -1,11 +1,28 @@
 import express from "express";
 import handlebars from "express-handlebars";
+import mongoose, { mongo } from 'mongoose'
 
 import routes from "../routes.js";
 import showRating from "./helpers/ratingHelper.js";
 
 const app = express();
-//
+
+//Db config
+try{
+  const uri='mongodb://localhost:27017/magic-movies'
+  await mongoose.connect(uri)
+  console.log('conected success on DB');
+  
+}catch(err){
+  console.log('cannot conect to DB');
+  
+  console.log(err.message);
+  
+   
+}
+
+
+//handlebars
 app.engine(
   "hbs",
   handlebars.engine({
@@ -18,9 +35,15 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", "./src/views");
 
+
+//express config
 app.use("/static", express.static("src/public"));
 app.use(express.urlencoded({ extended: false })); //learn xpres to parse form data
 //
+
+
+
+//rouutes
 app.use(routes); /////
 
 //
