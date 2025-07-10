964acc4bc7c9cebf2c43a2bb1cb808f3dcc91151
Svetozar Stoev <zarkoto23@gmail.com>
2025-07-10 17:44:40 +0300

    feat: add authentication routes with authController

--- Back-End-Module/Workshop-Movie-Magic/routes.js
+++ Back-End-Module/Workshop-Movie-Magic/routes.js
@@ -1,3 +1,4 @@
+import authController from "./src/controllers/authController.js";
 import castController from "./src/controllers/castController.js";
 import homeController from "./src/controllers/homeController.js";
 import movieControllerRouter from "./src/controllers/movieController.js";
@@ -9,5 +10,6 @@ const routes = Router();
 routes.use(homeController);
 routes.use("/movies", movieControllerRouter);
 routes.use('/casts',castController)
+routes.use('/auth', authController)
 
 export default routes;
