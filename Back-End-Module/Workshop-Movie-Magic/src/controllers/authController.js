fd360d4984c6111433c9afb9062bea3ed5bb4660
Svetozar Stoev <zarkoto23@gmail.com>
2025-07-12 16:25:10 +0300

    feat: implement user registration functionality with form handling and service integration

--- Back-End-Module/Workshop-Movie-Magic/src/controllers/authController.js
+++ Back-End-Module/Workshop-Movie-Magic/src/controllers/authController.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
+import authService from "../services/authService.js";
 
 const authController=Router()
 
 authController.get('/register',(req, res)=>{
     res.render('auth/register')
 })
+
+authController.post('/register',async(req,res)=>{
+    const userData=req.body
+    console.log(userData);
+    await authService.register(userData)
+    res.end()
+    
+})
+
+
 
 export default authController
---
+++ Back-End-Module/Workshop-Movie-Magic/src/services/authService.js
@@ -0,0 +1,10 @@
+import User from "../models/User.js"
+
+
+export default{
+
+    register(userData){
+        return User.create(userData)
+        
+    }
+}
