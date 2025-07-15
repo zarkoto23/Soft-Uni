d842b97819b12d6e6e457bcb66849246861a650a
Svetozar Stoev <zarkoto23@gmail.com>
2025-07-15 15:59:53 +0300

    feat: add logout functionality with route and update main layout for logout link

--- Back-End-Module/Workshop-Movie-Magic/src/controllers/authController.js
+++ Back-End-Module/Workshop-Movie-Magic/src/controllers/authController.js
@@ -29,4 +29,10 @@ authController.post("/login", async (req, res) => {
   }
 });
 
+authController.get('/logout',(req, res)=>{
+    res.clearCookie('auth')
+    res.redirect('/')
+
+})
+
 export default authController;
