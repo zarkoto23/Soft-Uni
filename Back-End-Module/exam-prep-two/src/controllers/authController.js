6f263aa23590c7827bc1d6f15ccd61c878076c95
Svetozar Stoev <zarkoto23@gmail.com>
2025-08-08 15:39:40 +0300

    add register page

---
+++ Back-End-Module/exam-prep-two/src/controllers/authController.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const authController=Router()
+
+authController.get('/register',(req,res)=>{
+res.render('auth/register')
+})
+
+export default authController
--- Back-End-Module/exam-prep-two/src/routes.js
+++ Back-End-Module/exam-prep-two/src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import homeController from "./controllers/homeController.js";
+import authController from "./controllers/authController.js"
 
 const routes=Router()
 
 routes.use(homeController)
+routes.use('/auth', authController)
 
 
 export default routes
