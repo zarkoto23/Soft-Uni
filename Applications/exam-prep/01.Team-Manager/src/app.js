59cad60eb9b9cf513e1f5db18dac73815114aa17
Svetozar Stoev <zarkoto23@gmail.com>
2025-08-14 10:51:59 +0300

    add utils module

--- Applications/exam-prep/01.Team-Manager/src/app.js
+++ Applications/exam-prep/01.Team-Manager/src/app.js
@@ -1,1 +1,26 @@
-console.log('ahdshah');
+import page from "../node_modules/page/page.mjs"
+
+
+import showCreate from "./views/create.js";
+import showDashboard from "./views/dashboard.js";
+import showDetails from "./views/details.js";
+import showEdit from "./views/edit.js";
+import showHome from "./views/home.js";
+import showLogin from "./views/login.js";
+import showProfile from "./views/profile.js";
+import showRegister from "./views/register.js";
+
+
+
+page('/', showHome)
+page('/login', showLogin)
+page('/register', showRegister)
+
+page('/dashboard', showDashboard)
+page('/create', showCreate)
+page('/profile', showProfile)
+
+page('/details/:id', showDetails)
+page('/edit/:id', showEdit)
+
+page()
---
+++ Applications/exam-prep/01.Team-Manager/src/utils.js
@@ -0,0 +1,26 @@
+
+
+
+function getToken() {
+  return localStorage.getItem("token");
+}
+
+function getUserId() {
+  return localStorage.getItem("userId");
+}
+
+function setUser(userData) {
+  localStorage.setItem("token", userData.accessToken);
+  localStorage.setItem("userId", userData._id);
+  localStorage.setItem("email", userData.email);
+}
+
+
+function removeUser(){
+    localStorage.removeItem('token')
+    localStorage.removeItem('userId')
+    localStorage.removeItem('email')
+
+}
+
+
